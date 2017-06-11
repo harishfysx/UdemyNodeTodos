b555d6ff6e9707c4ba813f32c4cee6a8e3c1ea43
root <root@zenbership126.harishfysx.bpm>
2017-06-10 23:24:53 -0400

    Object ID

--- playground/mondb.connect.js
+++ playground/mondb.connect.js
@@ -1,6 +1,14 @@
-const MognoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb'); // This is equilant to above line
 
-MognoClient.connect('mongodb://127.0.0.1:27017/TodoApp',(err,db) =>{
+/*
+EC6 standard destructuring
+var user = {name:'Harish',age:30}
+var {name} = user;
+console.log(name); //prints Harish
+*/
+
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp',(err,db) =>{
    if(err){
     return console.log('Unable to connect mongodb server')
    }
@@ -16,15 +24,16 @@ MognoClient.connect('mongodb://127.0.0.1:27017/TodoApp',(err,db) =>{
   //      console.log(JSON.stringify(result.ops,undefined, 2)) //ops object store all the documents that were inserted
   //  });
   //insert user
-  db.collection('Users').insertOne({
-    name: 'Harish',
-    age : 30
-  },(err,result) =>{ //insertOne take two objects
-      if(err){
-        return console.log('Unable to insert user',err);
-      }
-      console.log(JSON.stringify(result.ops,undefined, 2)); //ops object store all the documents that were inserted
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Harish',
+  //   age : 30
+  // },(err,result) =>{ //insertOne take two objects
+  //     if(err){
+  //       return console.log('Unable to insert user',err);
+  //     }
+  //     console.log(JSON.stringify(result.ops,undefined, 2)); //ops object store all the documents that were inserted
+  //     console.log(result.ops[0]._id.getTimestamp()); //returns the time stamp when object is created;
+  // });
 
    db.close();
 });
