e37bdd2585ca3a7611214d4a633d25e4d34f5423
root <root@zenbership126.harishfysx.bpm>
2017-06-11 11:17:55 -0400

    Mongoose Validatiors , defaults

--- server/server.js
+++ server/server.js
@@ -2,27 +2,52 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
-
+//Todo Model
 var Todo = mongoose.model('Todos',{
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim : true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
+  }
+});
+ //User Model
+var User = mongoose.model('Users',{
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim : true
   }
 });
 
+//save User
+var newUser = new User({
+   email : '  harish@gmail.com '
+})
+
+newUser.save().then((doc) =>{
+   console.log('Saved todo',JSON.stringify(doc,undefined,2));
+}, (e) =>{
+   console.log('Unable to save',e);
+});
+/*
 var newTodo = new Todo({
   text: 'Cook Dinner',
 });
 
 var otherTodo = new Todo({
-  text: 'Feed Cat',
-  completed :true,
-  completedAt : 123
+  text: 'Test',
+//  completed :true,
+  //completedAt : 123
 });
 
 otherTodo.save().then((doc) =>{
@@ -30,3 +55,4 @@ otherTodo.save().then((doc) =>{
 },(e) => {
   console.log('Unable to save',e);
 });
+*/
