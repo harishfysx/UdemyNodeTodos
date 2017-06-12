844cfa80604629336bcec812d27a8db8b250611c
root <root@zenbership126.harishfysx.bpm>
2017-06-11 20:48:35 -0400

    Created update end point

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 //user define environment variables
 var port = process.env.PORT || 3000;
 // yarn downloaded modules
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
+const _ = require('lodash');
 
 //local user defined imports
 var {
@@ -14,13 +16,16 @@ var {
 var {
     User
 } = require('./models/user');
-const {ObjectId} = require('mongodb');
+
 
 var app = express();
 
 //Middleware
 app.use(bodyParser.json());
 
+//Start Request handling
+
+//Home page handling
 app.get('/', function(req, res) {
     res.send('hello world')
 });
@@ -80,6 +85,35 @@ app.delete('/todos/:id',(req,res) =>{
   }).catch((e) =>{
     res.status(404).send();
   })
+
+});
+
+//PATCH todo/:id
+app.patch('/todos/:id',(req,res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  //check if completed set to true
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  //update
+  Todo.findByIdAndUpdate(id,{$set : body},{new : true}).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) =>{
+     res.status(404).send();
+  })
+
 
 });
 //start server
