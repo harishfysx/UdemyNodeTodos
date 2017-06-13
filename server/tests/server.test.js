cc486831a399a6a73029e422793bda9fac314de5
root <root@zenbership126.harishfysx.bpm>
2017-06-13 15:41:15 -0400

    added testcase for /delete token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -332,5 +332,25 @@ describe('POST /users/login', () => {
       });
   });
 }); //end users/login
+//start /users/me/tokens
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', usersTestData[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(usersTestData[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+//end /users/me/token
 
 //End USERS end points
